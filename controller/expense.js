848569c20685f2883ef120a8f15ca689467c6155
AnuraagChetia <anuraagchetia77@gmail.com>
2023-07-08 21:45:39 +0530

    making production ready

--- controller/expense.js
+++ controller/expense.js
@@ -1,5 +1,6 @@
 const Expense = require("../model/expense");
 const User = require("../model/user");
+const sequelize = require("../util/database");
 
 exports.getExpense = async (req, res) => {
   try {
@@ -11,41 +12,52 @@ exports.getExpense = async (req, res) => {
 };
 
 exports.postExpense = async (req, res) => {
+  const t = await sequelize.transaction();
   const { category, amount, note } = req.body;
   try {
-    const response = await Expense.create({
-      category: category,
-      amount: amount,
-      note: note,
-      userId: req.user.id,
-    });
+    const response = await Expense.create(
+      {
+        category: category,
+        amount: amount,
+        note: note,
+        userId: req.user.id,
+      },
+      {
+        transaction: t,
+      }
+    );
     const updatedTotalExpense = Number(req.user.totalExpense) + Number(amount);
     User.update(
       { totalExpense: updatedTotalExpense },
-      { where: { id: req.user.id } }
+      { where: { id: req.user.id }, transaction: t }
     );
+    await t.commit();
     res.status(200).json(response);
   } catch (error) {
     console.log(error);
+    await t.rollback();
     res.status(500).json({ err: error });
   }
 };
 
 exports.deleteExpense = async (req, res) => {
   try {
+    const t = await sequelize.transaction();
     const id = req.params.id;
     const exp = await Expense.findByPk(id);
     if (req.user.id === exp.userId) {
-      const result = await exp.destroy();
+      const result = await exp.destroy({ transaction: t });
       const updatedTotalExpense =
         Number(req.user.totalExpense) - Number(exp.amount);
       User.update(
         { totalExpense: updatedTotalExpense },
-        { where: { id: req.user.id } }
+        { where: { id: req.user.id }, transaction: t }
       );
+      await t.commit();
       res.status(200).json(result);
     }
   } catch (error) {
+    await t.rollback();
     res.status(500).json(error);
   }
 };
